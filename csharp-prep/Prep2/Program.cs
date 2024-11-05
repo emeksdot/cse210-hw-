d325007da8d42c5d0f755cb15b33bb33cdde3b34
emeksdot <emeksdot@yahoo.com>
2024-11-05 19:35:31 +0100

    the prep 3 code worked with the looping construct

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string value = Console.ReadLine();
+        float grade = float.Parse(value);
+        char letter;
+
+        if (grade >= 90)
+        {
+            letter = 'A';
+            
+        }
+        else if (grade >= 80)
+        {
+            letter = 'B';
+        }
+        else if (grade >= 70)
+        {
+            letter = 'C';
+        }
+        else if (grade >= 60)
+        {
+            letter = 'D';
+        }
+        else 
+        {
+            letter = 'F';
+        }
+        Console.WriteLine(letter);
+        if(grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed the Course!");
+        } else
+        {
+            Console.WriteLine("So sorry you did not pass the course. Better luck next time.");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = 5;
+        Console.WriteLine("Guess a number: ");
+        string response = Console.ReadLine();
+        int responeNumber = int.Parse(response);
+
+        if (responeNumber > magicNumber)
+        {
+            Console.WriteLine("Your guess is higher.");
+        }
+        if (responeNumber < magicNumber)
+        {
+            Console.WriteLine("Your guess is lower.");
+        }
+        if (responeNumber == magicNumber)
+        {
+            Console.WriteLine("Your guess is correct.");
+        }
     }
 }

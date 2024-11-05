6e66c5d0ea0c8d01481cdc8cdf8e920888adcd04
emeksdot <emeksdot@yahoo.com>
2024-11-05 19:55:48 +0100

    added a while loop and it worked

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,22 +5,33 @@ class Program
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Prep3 World!");
+
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
         int magicNumber = 5;
+        
         Console.WriteLine("Guess a number: ");
         string response = Console.ReadLine();
         int responeNumber = int.Parse(response);
 
-        if (responeNumber > magicNumber)
+        while (responeNumber != magicNumber)
         {
-            Console.WriteLine("Your guess is higher.");
-        }
-        if (responeNumber < magicNumber)
-        {
-            Console.WriteLine("Your guess is lower.");
+            
+            if (responeNumber > magicNumber)
+            {
+                Console.WriteLine("Your guess is higher.");
+            }
+            if (responeNumber < magicNumber)
+            {
+                Console.WriteLine("Your guess is lower.");
+            }
+             response = Console.ReadLine();
+             responeNumber = int.Parse(response);
         }
         if (responeNumber == magicNumber)
-        {
-            Console.WriteLine("Your guess is correct.");
-        }
+            {
+                Console.WriteLine("Your guess is correct.");
+            }
     }
 }

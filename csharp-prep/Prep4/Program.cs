51f8b45d8775b766b44abe656138b119824872ec
emeksdot <emeksdot@yahoo.com>
2024-11-07 18:15:22 +0100

    created the list of integers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
     }
 }

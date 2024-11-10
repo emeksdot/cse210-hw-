9b6a912b06c290e7a155f297aac7fd3644d7a5dc
emeksdot <emeksdot@yahoo.com>
2024-11-10 11:47:45 +0100

    Completed the Resume.cs class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume firstResume = new Resume();
+
+        firstResume._name = "Allison Rose";
+
+        firstResume._jobs.Add(job1);
+        firstResume._jobs.Add(job2);
+
+        firstResume.Display();
     }
 }

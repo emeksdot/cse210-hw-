2d195c6403fc3ad68f63e2284b85e324318da931
emeksdot <emeksdot@yahoo.com>
2024-11-11 15:57:25 +0100

    finally corrected the resume file by adding the missing parentheses

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -5,12 +5,12 @@ public class Resume
     public string _name;
     public List<Job> _jobs = new List<Job>();
 
-    public void Display
+    public void Display()
     {
-        System.Console.WriteLine($"Name: {_name}");
-        System.Console.WriteLine($"Jobs: ");
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Jobs: ");
 
-        foreach (Job job in _jobs)
+        foreach(Job job in _jobs)
         {
             job.Display();
         }

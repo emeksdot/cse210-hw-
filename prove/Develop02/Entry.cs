7a1129c6edca7a6c00ac830f31cb6c90609c9763
emeksdot <emeksdot@yahoo.com>
2024-11-16 19:50:35 +0100

    created the Journal, Entry, and PrompGenerator classes and they compiled without errors

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Entry
+{
+    string _date = DateTime.Now.ToShortDateString();
+    string _promptText;
+    string _entryText;
+
+    public void Display()
+    {
+        
+    }
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Journal
+{
+    // public List<Entry> _entries = new List<Entry>();
+
+    // public void AddEntry(Entry newEntry)
+    // {
+
+    //     foreach(Job job in _jobs)
+    //     {
+    //         job.Display();
+    //     }
+
+    // }
+
+    public void DisplayAll()
+    {
+
+        System.Console.WriteLine("Hello Ruler");
+
+    }
+
+    public void SaveToFile()
+    {
+
+        System.Console.WriteLine("Rule2");
+
+    }
+
+    public void LoadFromFile()
+    {
+
+       System.Console.WriteLine("Rule3");
+
+    }
+}

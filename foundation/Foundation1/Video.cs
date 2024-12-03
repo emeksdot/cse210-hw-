577832b20b1f0af079e23c927fab753a84132334
emeksdot <emeksdot@yahoo.com>
2024-12-03 11:25:59 +0100

    created the member variables for the Video class

---
+++ foundation/Foundation1/Video.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    
+}

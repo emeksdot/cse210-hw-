1853b4a34f3b9349edcaf764d2fabd4e58ce0605
emeksdot <emeksdot@yahoo.com>
2024-12-05 11:39:19 +0100

    created the Comment objects

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation1 World!");
+        List<Video> _videos = new List<Video>();
+        Video vid1 = new Video("Essential Pots", "Kitchen Appliances", 188);
+        Video vid2 = new Video("Rice Cooker", "", 236);
+        Video vid3 = new Video("Digital Alarms", "Aquatica", 197);
+        Video vid4 = new Video("L.E.D. Projector", "Mildred Electronics", 125);
     }
 }
--- foundation/Foundation1/Video.cs
+++ foundation/Foundation1/Video.cs
@@ -8,14 +8,14 @@ public class Video
     private string _title;
     private string _author;
     private int _length;
-    private List<Comment> _comments;
+    private List<Comment> _comments = new List<Comment>();
 
-    public Video(string title, string author, int length, List<Comment> comments)
+    public Video(string title, string author, int length)
     {
         _title = title;
         _author = author;
         _length = length;
-        _comments = comments;
+        _comments = new List<Comment>();
         //List<Comment>  _comments = new List<Comment>();
 
     }

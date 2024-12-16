135023adc752a66c4ec282019c167a9bb7de5366
emeksdot <emeksdot@yahoo.com>
2024-12-16 05:51:46 +0100

    made adjustments to the Goal class by removing the Abstract designations

--- prove/Develop06/Goal.cs
+++ prove/Develop06/Goal.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-public abstract class Goal
-{
-    public abstract void RecordEvent();
-    public abstract bool IsComplete();
-    public string GetDetailsString()
-    {
-        return "Returns a string.";
-    }
-    public abstract string GetStringRepresentation();
 
+public class Goal
+{
+    // Private fields
+    private string _shortName;
+    private string _description;
+    private string _points;
+
+    public Goal(string name, string description, string points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    // _________ ShortName ____________
+    public string GetShortName()
+    {
+        return _shortName;
+    }
+    public void SetShortName(string name)
+    {
+        _shortName = name;
+    }
+
+    // _________ Description __________
+    public string GetDescription()
+    {
+        return _description;
+    }
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    // ________ Points _________
+    public string GetPoints()
+    {
+        return _points;
+    }
+    public void SetPoints(string points)
+    {
+        _points = points;
+    }
 }
